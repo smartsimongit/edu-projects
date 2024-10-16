905f4427c1e69d9734a9310429279a17bf41891b
smartsimon <>
2024-10-16 15:18:39 +0200

    # literally most boring problem (fizzBuzz)

---
+++ simple-spring-boot-app/src/main/java/org/smart/simon/edu/simplespringbootapp/SimpleSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package org.smart.simon.edu.simplespringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootAppApplication.class, args);
+	}
+
+}
