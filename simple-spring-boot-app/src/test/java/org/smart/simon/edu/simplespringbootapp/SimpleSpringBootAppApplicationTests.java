4f14850501582140c540ae8216e48587683b2cce
smartsimongit <47315266+smartsimongit@users.noreply.github.com>
2024-10-16 15:19:33 +0200

    Temp (#4)
    * # literally most boring problem (fizzBuzz)

---
+++ simple-spring-boot-app/src/test/java/org/smart/simon/edu/simplespringbootapp/SimpleSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smart.simon.edu.simplespringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
