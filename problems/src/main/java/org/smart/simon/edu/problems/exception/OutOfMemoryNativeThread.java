0586f65e7bd98ca9d3517e4b603f0d797b9f629b
smartsimon <>
2024-10-30 00:00:34 +0100

    # exceptions

--- problems/src/main/java/org/smart/simon/edu/problems/exception/OutOfMemoryNativeThread.java
+++ problems/src/main/java/org/smart/simon/edu/problems/exception/OutOfMemoryNativeThread.java
@@ -1,10 +1,21 @@
 package org.smart.simon.edu.problems.exception;
 
-import ch.qos.logback.core.encoder.JsonEscapeUtil;
+import static java.util.stream.IntStream.range;
 
+//OutOfMemoryError: unable to create native thread
 public class OutOfMemoryNativeThread {
     public static void main(String[] args) {
         int size = 1_000_000;
+        range(1, size).forEach(OutOfMemoryNativeThread::createThread);
+    }
 
+    private static void createThread(int i) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(10_000);
+            } catch (InterruptedException e) {
+            }
+        }).start();
+        System.out.printf("Created %d threads\n", i);
     }
 }
