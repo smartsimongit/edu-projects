3bb9a39bf6c6a65207e8773f98b6066d5d7bf1ee
smartsimon <>
2024-12-18 19:26:14 +0100

    # grpc

---
+++ gRPC-simple-app/src/main/java/org/smart/simon/edu/grpcsimpleapp/GRpcSimpleAppApplication.java
@@ -0,0 +1,12 @@
+package org.smart.simon.edu.grpcsimpleapp;
+
+
+
+//@SpringBootApplication
+public class GRpcSimpleAppApplication {
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(GRpcSimpleAppApplication.class, args);
+//    }
+
+}
---
+++ gRPC-simple-app/src/test/java/org/smart/simon/edu/grpcsimpleapp/GRpcSimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smart.simon.edu.grpcsimpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcSimpleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
