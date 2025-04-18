6695923db282c972c8ff11efaac29a92ac1e3fdc
smartsimon <>
2025-04-18 09:22:24 +0300

    # Medium167TwoSumIIInputArrayIsSorted

--- problems/src/main/java/org/smart/simon/edu/problems/leetcode/Medium167TwoSumIIInputArrayIsSorted.java
+++ problems/src/main/java/org/smart/simon/edu/problems/leetcode/Medium167TwoSumIIInputArrayIsSorted.java
@@ -1,11 +1,17 @@
 package org.smart.simon.edu.problems.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Medium167TwoSumIIInputArrayIsSorted {
     public int[] twoSum(int[] numbers, int target) {
-        int y = numbers.length-1;
-        for (int i = 0; i<numbers.length; i++) {
-            if (numbers[i]+numbers[y] == target)
-                return new int[]{numbers[i],numbers[y]};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int temp = target - numbers[i];
+            if (map.containsKey(temp)) {
+                return new int[]{map.get(temp), i + 1};
+            }
+            map.put(numbers[i], i + 1);
         }
         return new int[0];
     }
